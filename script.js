809a9ed93836c755fd0a9ef9cd687a4c581b11b2
alihanafy10 <113368324+alihanafy10@users.noreply.github.com>
2026-04-24 03:37:48 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -35,8 +35,8 @@ const userAvatar = document.getElementById('userAvatar');
 function getAvatar(name) {
     if (!name) return '👤';
     const lowerName = name.toLowerCase();
-    if (lowerName === 'ali') return '<img src="assets/wolf.png" class="avatar-img" alt="Ali" />';
-    if (lowerName === 'zoza') return '<img src="assets/bee.png" class="avatar-img" alt="Zoza" />';
+    if (lowerName === 'ali') return '<img src="assets/wolf.jpg" class="avatar-img" alt="Ali" />';
+    if (lowerName === 'zoza') return '<img src="assets/bee.jpg" class="avatar-img" alt="Zoza" />';
     return '👤';
 }
 const logoutBtn = document.getElementById('logoutBtn');
